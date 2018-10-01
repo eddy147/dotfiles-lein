6d662339a27dfb58a8dc0d11e03c628b0e637a85
Eddy de Boer <eddy147@gmail.com>
2018-10-01 19:38:25 +0200

    Updated with latest

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,5 @@
-{:user {:dependencies [[org.clojure/tools.namespace "0.2.3"]
+{:user {:dependencies [[org.clojure/clojure "1.8.0"]
+                       [org.clojure/tools.namespace "0.2.11"]
                        [org.clojure/tools.nrepl "0.2.13"]
                        [cljfmt "0.5.7"]
                        [spyscope "0.1.3"]
@@ -7,13 +8,14 @@
                      ; try/catch to workaround an issue where `lein repl` outside a project dir
                      ; will not load reader literal definitions correctly:
                      (try (require 'spyscope.core)
-                       (catch RuntimeException e))]
+                          (catch RuntimeException e))]
         :plugins [[lein-pprint "1.1.1"]
-                  [lein-beanstalk "0.2.6"]
-                  [lein-cljfmt "0.5.7"]
+                  [lein-ancient "0.6.15"]
+                  [lein-cljfmt "0.6.1"]
                   [lein-clojars "0.9.1"]
-                  [lein-create-template "0.1.1"]
-                  [lein-marginalia "0.7.1"]
-                  [lein-exec "0.3.1"]
-                  [lein-midje "3.1.1"]
-                  [lein-kibit "0.0.8"]]}}
+                  [lein-create-template "0.2.0"]
+                  [lein-marginalia "0.9.1"]
+                  [lein-exec "0.3.7"]
+                  [lein-midje "3.2.0"]
+                  [lein-kibit "0.1.6"]
+                  [lein-gorilla "0.4.0"]]}}
